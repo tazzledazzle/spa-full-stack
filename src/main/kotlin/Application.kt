a674a72e9dc80122ef2e23d808c4e7ad0ac855cc
Terence Schumacher <terenceschumacher@gmail.com>
2025-03-06 12:00:19 -0800

    passing session data stop to switch to pebble

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -4,39 +4,30 @@ import UserSession
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.engine.embeddedServer
 import io.ktor.server.html.*
+import io.ktor.server.netty.Netty
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.sessions.sessions
 import io.ktor.util.reflect.*
 import kotlinx.html.*
 
-//fun main(args: Array<String>) {
+fun main(args: Array<String>) {
 //    io.ktor.server.netty.EngineMain.main(args)
 
-//    embeddedServer(Netty, port = 8080, host = "localhost") {
-//        install(Sessions) {
-//            cookie<FlashData>("FLASH_DATA") {
-//                cookie.path = "/cookies"
-//                serializer = KotlinxSessionSerializer(Json {
-//                    prettyPrint = true
-//                    isLenient = true
-//                    ignoreUnknownKeys = true
-//                })
-//            }
-//        }
+    embeddedServer(Netty, port = 8080, host = "localhost") {
+        configureForms()
+    }.start(wait = true)
+}
 
-//        main()
-//    }.start(wait = true)}
-
-fun Application.main() {
+fun Application.configureForms() {
     install(Authentication) {
         session<UserSession>("auth-session") {
             validate { session ->
                 if (session.name.startsWith("jet")) {
                     session
-                }
-                else {
+                } else {
                     null
                 }
             }
@@ -70,7 +61,7 @@ fun Application.main() {
             }
             post("/login") {
                 val userName = call.principal<UserIdPrincipal>()?.name.toString()
-                call.sessions.set("user", UserSession(name = userName, count =  1))
+                call.sessions.set("user", UserSession(name = userName, count = 1))
                 call.respondRedirect("/hello")
             }
         }
@@ -78,7 +69,11 @@ fun Application.main() {
 
             call.respondHtml {
                 body {
-                    form(action = "/login", encType = FormEncType.applicationXWwwFormUrlEncoded, method = FormMethod.post) {
+                    form(
+                        action = "/login",
+                        encType = FormEncType.applicationXWwwFormUrlEncoded,
+                        method = FormMethod.post
+                    ) {
                         p {
                             +"Username:  "
                             textInput(name = "username")
