bd4e761c29ba79ff137131b052cebdcea9abf8c8
Terence Schumacher <terenceschumacher@gmail.com>
2025-07-07 15:01:50 -0700

    added react and test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
-    kotlin("multiplatform") version "2.2.0"
+    kotlin("multiplatform") version "2.1.0"
+}
+repositories {
+    mavenCentral()
+    google()
 }
 
 kotlin {
@@ -12,15 +16,14 @@ kotlin {
     sourceSets {
         val jsMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+                implementation(npm("react", "^18.2.0"))
+                implementation(npm("react-dom", "^18.2.0"))
             }
         }
 
         commonTest {
             dependencies {
                 implementation(kotlin("test"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
             }
         }
     }
